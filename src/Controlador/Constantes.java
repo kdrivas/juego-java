87fc98085321d65f6a147c276467cf5c2eb6b00b
KervyRivas <k.rivas@pucp.pe>
2016-11-15 00:30:54 -0500

    añadi constantes

---
+++ src/Controlador/Constantes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Constantes {
+    public static final int ANCHO_VENTANA = 1100;
+    public static final int ALTO_VENTANA = 600;
+    
+    public static final int CENTRO_VENTANA_X = ANCHO_VENTANA / 2;
+    public static final int CENTRO_VENTANA_Y = ALTO_VENTANA / 2;
+    
+    public static final int ANCHO_JUGADOR = 48;
+    public static final int ALTO_JUGADOR = 64;
+}
